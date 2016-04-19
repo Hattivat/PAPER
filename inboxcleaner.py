bfbde056f46032392386e6b858c2d4edb7405be3
Hattivat <jan.janiczek@yahoo.com>
2016-04-19 20:01:02 +0200

    Discovered that if there are more than 100 'spam' mails in the inbox, outlook doesn't let me list all of them, reverted to a simpler but more robust method; some formatting fixes

--- inboxcleaner.py
+++ inboxcleaner.py
@@ -9,37 +9,37 @@ import win32com.client as client
 
 SHAREDMAILBOX = "xxxxxxxxxx"
 TRASHFOLDER = "Untitled"
+ROBOMAIL = "xxxxxxxxxxxxx"
 
-def clean_mail(mailbox=SHAREDMAILBOX, trashname=TRASHFOLDER):
+
+def clean_mail(mailbox=SHAREDMAILBOX, trashname=TRASHFOLDER, origin=ROBOMAIL):
     """Takes a mailbox and moves any PAMSNoReply mails found in its inbox to
     the trash folder of the provided name.
-    
+
     takes:
     mailbox: a string containing the name of shared mailbox to be cleaned
     trashname: the name of the folder used for NoReply mail on that mailbox
-    
+
     returns: nothing
     prints: the amount of emails moved (default 0)"""
-    
+
     outlook = client.Dispatch("Outlook.Application")
     outlookMAPI = outlook.GetNamespace("MAPI")
     recipient = outlookMAPI.CreateRecipient(mailbox)
     recipient.Resolve()
-    #Find the folders needed
+    # Find the folders needed
     inbox = outlookMAPI.GetSharedDefaultFolder(recipient, 6)
     parentfolder = inbox.Parent
     trashfolder = parentfolder.Folders(trashname)
     messages = inbox.Items
-    #Only used comprehension to see if it would speed things up; no noticeable
-    #difference, kept it out of inertia.
-    noreplymsgs = [msg for msg in messages if msg.SenderName == "PAMSNoReply@gsk.com"]
+
     counter = 0
-    for item in noreplymsgs:
-        item.UnRead = False
-        item.Move(trashfolder)
-        counter += 1
+    for item in messages:
+        if item.SenderName == origin:
+            item.UnRead = False
+            item.Move(trashfolder)
+            counter += 1
     print("{0} noreply emails moved.".format(counter))
-    
+
 if __name__ == "__main__":
     clean_mail()
-    
