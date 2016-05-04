3bb1cbea1b0b5b738cb07494540d753e7bfad352
Hattivat <jan.janiczek@yahoo.com>
2016-05-05 00:24:59 +0200

    A more secure version of passgen

--- passgen.py
+++ passgen.py
@@ -1,10 +1,19 @@
 import random
+from os import urandom
 
-letters = 'abcdefghjkmnqprstuvwx'
-passoptions = letters + letters.upper() + '123456789' * 2
+acceptable_chars = 'abcdefghjkmnqprstuvwx23456789ABCDEFGHJKMNQPRSTUVWX23456789'
 
-def pass_gen(size=8):
-    print(''.join(random.choice(passoptions) for char in range(size)))
+
+def pass_gen(size=12):
+    password = ''
+    for character in range(size):
+        randomness = urandom(1)
+        password.join(acceptable_chars[randomness % 58])
+    print(password)
+
+
+def old_pass_gen(size=8):
+    print(''.join(random.choice(acceptable_chars) for char in range(size)))
 
 if __name__ == "__main__":
     pass_gen()
