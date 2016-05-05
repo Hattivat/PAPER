a76fe728729175a13ef5e38e36704e90bbdbb30d
Hattivat <jan.janiczek@yahoo.com>
2016-05-05 03:20:07 +0200

    added comments

--- passgen.py
+++ passgen.py
@@ -1,10 +1,15 @@
 import random
 from os import urandom
 
+# Omitting potentially confusing characters, such as 0, O, o, i, l, I, 1
 acceptable_chars = 'abcdefghjkmnqprstuvwx23456789ABCDEFGHJKMNQPRSTUVWX23456789'
 
 
 def pass_gen(size=12):
+    """ Generates a reasonably secure password using the OS's built-in RNG,
+    converting the byte outputted by it into an integer and using that to pick
+    one of the 58 characters in the 'acceptable_chars' string.
+    """
     password = ''
     for i in range(size):
         randomness = int.from_bytes(urandom(1), byteorder='little')
